7943ff0167771100d3fee053932b1cd16e7117f0
Daniel Scott <djscott00@gmail.com>
2020-11-20 16:21:00 -0800

    Removed cached env folder and sqlite db.
    Added .gitignore entries.
    Updated requirements file and runtime. Updated accepted local hosts to be anything (I don't think we're super worried about security).

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g7te-i3k@hubgitsg)%#*c6e-^3^rnewzxu#vocw$=#u!77gr1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['reviews99.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
